7e0f6482a1866db663ced53243fcf4ba9bc13382
Ramon Donnell <rayn95@gmail.com>
2014-08-05 17:17:29 +1000

    support for non rev'd files

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ var gulp = require('gulp');
 var batchReplace = require('gulp-batch-replace');
 
 function relPath(base, filePath) {
-  var newPath = filePath.replace(base, '');
-  if (filePath !== newPath && newPath[0] === path.sep) {
-    return newPath.substr(1);
-  } else {
-    return newPath;
-  }
+	var newPath = filePath.replace(base, '');
+	if (filePath !== newPath && newPath[0] === path.sep) {
+		return newPath.substr(1);
+	} else {
+		return newPath;
+	}
 }
 
 
@@ -23,33 +23,36 @@ function relPath(base, filePath) {
  * instead of replacing in the stream, it replaces whats in replaceGlob
  */
 var plugin = function (options) {
-  var renames = [];
+	var renames = [];
 
-  return through.obj(function (file, enc, cb) {
-    if (file.isNull()) {
-      this.push(file);
-      return cb();
-    }
+	return through.obj(function (file, enc, cb) {
+			if (file.isNull()) {
+				this.push(file);
+				return cb();
+			}
 
-    if (file.isStream()) {
-      this.emit('error', new gutil.PluginError('gulp-replace-revs-in', 'Streaming not supported'));
-      return cb();
-    }
+			if (file.isStream()) {
+				this.emit('error', new gutil.PluginError('gulp-replace-revs-in', 'Streaming not supported'));
+				return cb();
+			}
 
-    if (file.revOrigPath) {
-      renames.push([relPath(path.resolve(file.revOrigBase), file.revOrigPath), options.prefix + '/' +  relPath(file.base, file.path)]);
-      this.push(file);
-    }
-    cb();
-  },
+			if (file.revOrigPath) {
+				renames.push([relPath(path.resolve(file.revOrigBase), file.revOrigPath), options.prefix + '/' +  relPath(file.base, file.path)]);
+				this.push(file);
+			}else{
+				renames.push([relPath(file.base, file.path), options.prefix + '/' +  relPath(file.base, file.path)]);
+				this.push(file);
+			}
+			cb();
+		},
 
-  function(cb) {
-	  gulp.src(options.replaceGlob, {base: './'})
-		  .pipe(batchReplace(renames))
-		  .pipe(gulp.dest('./'))
-	    .on('end', cb)
-		  .on('error', gutil.log);
-  });
+		function(cb) {
+			gulp.src(options.replaceGlob, {base: './'})
+				.pipe(batchReplace(renames))
+				.pipe(gulp.dest('./'))
+				.on('end', cb)
+				.on('error', gutil.log);
+		});
 };
 
 module.exports = plugin;
